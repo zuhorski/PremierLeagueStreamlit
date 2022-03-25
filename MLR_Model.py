c15fc3322febb59fb1f348821e69c7ef1b3d32e8
sabzuski <sabzuski@gmail.com>
2022-03-25 11:38:28 -0400

    removed/commented functions that aren't used

--- MLR_Model.py
+++ MLR_Model.py
@@ -1,54 +1,53 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from myClasses import Fixture_DF, Standings, year_season, Standings_V2, list_of_specific_files
+from myClasses import Fixture_DF, Standings_V2, list_of_specific_files
 
 
-def data_for_train_test(game):
-    ys = year_season()
-    year = ["_" + x[-7:-5] + "_" + x[-2:] for x in ys]
-
-    df = pd.DataFrame()
-    for ind, i in enumerate(ys):
-
-        data_orig = pd.read_excel(rf"C:\Users\sabzu\Documents\All EPL Project Files\Fixtures\{i}.xlsx", header=1)
-        # data_h = data_orig[data_orig["Wk"] <= game]
-
-        fix = Fixture_DF(data_orig)
-        s = Standings_V2(fix, game)
-        # s = Standings(fix)
-        halfway = s.standings
-        halfway["Team"] = halfway["Team"].astype(str) + str(year[ind])
-
-        rank = list(range(1, 21))
-        halfway["rank"] = rank
-
-        fix1 = Fixture_DF(data_orig)
-        st = Standings_V2(fix1, 38)
-        # st = Standings(fix1)
-
-        full = st.standings
-        full["Team"] = full["Team"].astype(str) + str(year[ind])
-        rank = list(range(1, 21))
-        full["rank"] = rank
-
-        endRank = []
-        for i in halfway["Team"]:
-            q = full[full["Team"] == i]
-            w = int(q["rank"])
-            endRank.append(w)
-        halfway["EndRank"] = endRank
-
-        df = halfway if len(df) == 0 else pd.concat([df, halfway])
-
-    df = df.convert_dtypes().reset_index(drop=True)
-    # df.to_csv("C:\\Users\\sabzu\\Documents\\PremierStreamlit\\Test_Train_datasets\\AggStandingsAtGameX.csv", header=True)
-    if game < 10:
-        game_string = '0' + str(game)
-        df.to_csv(fr"C:\Users\sabzu\Documents\PremierLeagueStreamlitProject\PremierLeagueStreamlit\Storage_of_Historical_Standings\game{game_string}_standings.csv", header=True)
-    else:
-        df.to_csv(fr"C:\Users\sabzu\Documents\PremierLeagueStreamlitProject\PremierLeagueStreamlit\Storage_of_Historical_Standings\game{game}_standings.csv", header=True)
-    return df
+# def data_for_train_test(game):
+#     ys = year_season()
+#     year = ["_" + x[-7:-5] + "_" + x[-2:] for x in ys]
+#
+#     df = pd.DataFrame()
+#     for ind, i in enumerate(ys):
+#
+#         data_orig = pd.read_excel(rf"C:\Users\sabzu\Documents\All EPL Project Files\Fixtures\{i}.xlsx", header=1)
+#         # data_h = data_orig[data_orig["Wk"] <= game]
+#
+#         fix = Fixture_DF(data_orig)
+#         s = Standings_V2(fix, game)
+#         # s = Standings(fix)
+#         halfway = s.standings
+#         halfway["Team"] = halfway["Team"].astype(str) + str(year[ind])
+#
+#         rank = list(range(1, 21))
+#         halfway["rank"] = rank
+#
+#         fix1 = Fixture_DF(data_orig)
+#         st = Standings_V2(fix1, 38)
+#         # st = Standings(fix1)
+#
+#         full = st.standings
+#         full["Team"] = full["Team"].astype(str) + str(year[ind])
+#         rank = list(range(1, 21))
+#         full["rank"] = rank
+#
+#         endRank = []
+#         for i in halfway["Team"]:
+#             q = full[full["Team"] == i]
+#             w = int(q["rank"])
+#             endRank.append(w)
+#         halfway["EndRank"] = endRank
+#
+#         df = halfway if len(df) == 0 else pd.concat([df, halfway])
+#
+#     df = df.convert_dtypes().reset_index(drop=True)
+#     # df.to_csv("C:\\Users\\sabzu\\Documents\\PremierStreamlit\\Test_Train_datasets\\AggStandingsAtGameX.csv", header=True)
+#     if game < 10:
+#         game_string = '0' + str(game)
+#         df.to_csv(fr"C:\Users\sabzu\Documents\PremierLeagueStreamlitProject\PremierLeagueStreamlit\Storage_of_Historical_Standings\game{game_string}_standings.csv", header=True)
+#     else:
+#         df.to_csv(fr"C:\Users\sabzu\Documents\PremierLeagueStreamlitProject\PremierLeagueStreamlit\Storage_of_Historical_Standings\game{game}_standings.csv", header=True)
+#     return df
 
 
 # This will have to be updated when there is a new current season
@@ -93,7 +92,7 @@ def predicted_PointsAndStandingsByModel(game):
             break
         skip_historical = False
     if not skip_historical:
-        df = data_for_train_test(game)
+        df = current_season_data_for_model(game, df)
 
     # Model
     X = df.iloc[:, [4,6,7]].values
@@ -135,7 +134,7 @@ def predicted_PointsAndStandingsByModel(game):
             break
         skip_current = False
     if not skip_current:
-        current_year_prediction = current_season_data_for_model(game)
+        current_year_prediction = current_season_data_for_model(game, current_year_prediction)
 
     # current_year_prediction = current_season_data_for_model(game)
     explanatory = current_year_prediction.iloc[:, [4,6,7]].values
@@ -153,42 +152,42 @@ def predicted_PointsAndStandingsByModel(game):
 
 
 # Same as data_for_train_test except there is no CSV
-def allTimeStandingsDataAtGameX(game):
-    ys = year_season()
-    year = ["_" + x[-7:-5] + "_" + x[-2:] for x in ys]
-
-    df = pd.DataFrame()
-    for ind, i in enumerate(ys):
-
-        data_orig = pd.read_excel(rf"C:\Users\sabzu\Documents\All EPL Project Files\Fixtures\{i}.xlsx", header=1)
-        # data_h = data_orig[data_orig["Wk"] <= game]
-
-        fix = Fixture_DF(data_orig)
-        s = Standings_V2(fix, game)
-        # s = Standings(fix)
-        halfway = s.standings
-        halfway["Team"] = halfway["Team"].astype(str) + str(year[ind])
-
-        rank = list(range(1, 21))
-        halfway["rank"] = rank
-
-        fix1 = Fixture_DF(data_orig)
-        st = Standings_V2(fix1, 38)
-        # st = Standings(fix1)
-
-        full = st.standings
-        full["Team"] = full["Team"].astype(str) + str(year[ind])
-        rank = list(range(1, 21))
-        full["rank"] = rank
-
-        endRank = []
-        for i in halfway["Team"]:
-            q = full[full["Team"] == i]
-            w = int(q["rank"])
-            endRank.append(w)
-        halfway["EndRank"] = endRank
-
-        df = halfway if len(df) == 0 else pd.concat([df, halfway])
-
-    df = df.convert_dtypes().reset_index(drop=True)
-    return df
+# def allTimeStandingsDataAtGameX(game):
+#     ys = year_season()
+#     year = ["_" + x[-7:-5] + "_" + x[-2:] for x in ys]
+#
+#     df = pd.DataFrame()
+#     for ind, i in enumerate(ys):
+#
+#         data_orig = pd.read_excel(rf"C:\Users\sabzu\Documents\All EPL Project Files\Fixtures\{i}.xlsx", header=1)
+#         # data_h = data_orig[data_orig["Wk"] <= game]
+#
+#         fix = Fixture_DF(data_orig)
+#         s = Standings_V2(fix, game)
+#         # s = Standings(fix)
+#         halfway = s.standings
+#         halfway["Team"] = halfway["Team"].astype(str) + str(year[ind])
+#
+#         rank = list(range(1, 21))
+#         halfway["rank"] = rank
+#
+#         fix1 = Fixture_DF(data_orig)
+#         st = Standings_V2(fix1, 38)
+#         # st = Standings(fix1)
+#
+#         full = st.standings
+#         full["Team"] = full["Team"].astype(str) + str(year[ind])
+#         rank = list(range(1, 21))
+#         full["rank"] = rank
+#
+#         endRank = []
+#         for i in halfway["Team"]:
+#             q = full[full["Team"] == i]
+#             w = int(q["rank"])
+#             endRank.append(w)
+#         halfway["EndRank"] = endRank
+#
+#         df = halfway if len(df) == 0 else pd.concat([df, halfway])
+#
+#     df = df.convert_dtypes().reset_index(drop=True)
+#     return df
